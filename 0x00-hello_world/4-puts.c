ea9d049288a3836ff79456a091951deb22b75f0c
christophe210 <christophe21445@gmail.com>
2023-06-15 13:31:33 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: Always 0 (0-executed sucessfully)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
