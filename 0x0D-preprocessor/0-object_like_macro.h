e3bc44151c7727625e319d8c36b88340eca56e0e
christophe210 <christophe21445@gmail.com>
2023-07-17 05:54:34 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File - 0-object_like_macro.h
+ * Description - header file that defines a macro named SIZE
+ * as an abbreviation from the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
